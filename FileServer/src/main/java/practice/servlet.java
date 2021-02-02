1f0af0df9352a67ea585da3f5222d4ea6d1077e4
khan06-bhopal <69720951+khan06-bhopal@users.noreply.github.com>
2021-02-02 02:39:18 -0800

    Add files via upload

---
+++ FileServer/src/main/java/practice/servlet.java
@@ -0,0 +1,153 @@
+package practice;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+public class servlet extends HttpServlet {
+	
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try{
+			ServletFileUpload sf  =new ServletFileUpload(new DiskFileItemFactory());
+			List<FileItem> multifiles = sf.parseRequest(request);
+			for(FileItem item:multifiles){
+				item.write(new File("/./"+item.getName()));
+			}
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		}
+		}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
---
+++ FileServer/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<form action="upload" method="post" enctype="multipart/form-data">
+<input type="file" name="file" multiple />
+<input type="submit" name="UPLOAD"/>
+</form>
+</body>
+</html>
